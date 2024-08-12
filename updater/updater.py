1477f31c46e6a96792a20ce0ca03bb9c6c4bd61d
Alberto Olivi <olivialberto02@gmail.com>
2024-08-12 18:20:42 -0400

    Updater 0.2

--- updater/updater.py
+++ updater/updater.py
@@ -42,13 +42,36 @@ def normal_update(new_data, old_data):
     check_files(new_data)
     to_folders = new_data['to_update']['folders']
     to_files = new_data['to_update']['files']
-    
-    for folder in to_folders:
+    print()
+    try:
+        for folder in to_folders:
+            check = PARENT_FOLDER + folder
+            if not os.path.exists(check) or not os.path.isdir(check):
+                print(f"Creating folder {check}.")
+                os.makedirs(check)
+        for file in to_files:
+            check = PARENT_FOLDER + file
+            if os.path.exists(check) and os.path.isfile(check):
+                print(f"Updating file {check}.")
+                url = new_data['github']['raw_link'] + file
 
-
+                response = requests.get(url)
+                if response.status_code == 200:
+                    content = response.text
+                    with open(check, 'w') as f:
+                        f.write(content)
+                else:
+                    print(f"Can't get request for updating {check}. Status code: {response.status_code}")
+                    raise Exception
+    except:
+        print('CRITICAL ERROR while updating.. Stopping..')
+        return None
+    return True
 
 
 def UPDATER_START():
+    status = None
+
     new_data = get_data(DATA)
     if new_data is None:
         return None
@@ -62,11 +85,15 @@ def UPDATER_START():
     if old_version == new_version:
         print("No new update found..")
         return None
+    
     elif (old_version + 0.1) == new_version:
-        print('Normal Update starting...')
-        normal_update(new_data, old_data)
+        print('Update starting...')
+        status = normal_update(new_data, old_data)
     elif (old_version + 0.1) < new_version:
         print('Program is two versions old. Downloading latest files...')
     else:
         print('Error found with version. Downloading latest files...')
+    
+    if status is not False and status is not None:
+        print("Updating successful!")
     
