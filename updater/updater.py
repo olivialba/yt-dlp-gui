94cad414ea5429dc5f748820fff90ac6bd4922c1
Alberto Olivi <olivialberto02@gmail.com>
2024-08-12 17:35:08 -0400

    Changed data for updating

--- updater/updater.py
+++ updater/updater.py
@@ -1,9 +1,13 @@
 import requests, zipfile, json, os, io
 
+
+def create_folders():
+    pass
+
 def UPDATER_START():
     with open("data.json", 'r') as file:
         data = json.load(file)
     dest = "../"
-    github = data['github']
+    anchor = data['github']['raw_link']
     folders = data['content']['folders']
     files = data['content']['files']
